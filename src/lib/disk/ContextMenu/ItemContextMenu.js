87611184ac1fb39790991f28864cc4d3e31d201b
rookie1989 <nichunlong@gmail.com>
2025-02-06 23:50:00 +0800

    优化代码

--- src/lib/disk/ContextMenu/ItemContextMenu.js
+++ src/lib/disk/ContextMenu/ItemContextMenu.js
@@ -1,67 +1,71 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Divider } from 'antd';
-import cn from 'classnames';
-import './index.less';
+import React, { useEffect, useRef, useState } from "react";
+import { Divider } from "antd";
+import cn from "classnames";
+import "./index.less";
 
-export default props => {
-	const { visible, left, top, contextMenuList, onContextMenuClick, onCancel } = props;
-	const itemContextRef = useRef();
-	const [position, setPosition] = useState({});
+export default (props) => {
+  const { visible, left, top, contextMenuList, onContextMenuClick, onCancel } =
+    props;
+  const itemContextRef = useRef();
+  const [position, setPosition] = useState({});
 
-	const calcPosition = (({ left, top }, { width, height }) => {
-		if (left + width > window.innerWidth) {
-			left = window.innerWidth - width;
-		}
+  const calcPosition = ({ left, top }, { width, height }) => {
+    if (left + width > window.innerWidth) {
+      left = window.innerWidth - width;
+    }
 
-		if (top + height > window.innerHeight) {
-			top = window.innerHeight - height;
-		}
+    if (top + height > window.innerHeight) {
+      top = window.innerHeight - height;
+    }
 
-		return {
-			left: left || '-9999px',
-			top: top || '-9999px'
-		};
-	});
+    return {
+      left: left || "-9999px",
+      top: top || "-9999px"
+    };
+  };
 
-	useEffect(() => {
-		const current = itemContextRef.current;
-		if (current) {
-			setPosition(calcPosition(props, current.getBoundingClientRect()));
-		}
-	}, [left, top, visible]);
+  useEffect(() => {
+    const current = itemContextRef.current;
+    if (current) {
+      setPosition(calcPosition(props, current.getBoundingClientRect()));
+    }
+  }, [left, top, visible]);
 
-	return (
-		<div
-			className="disk-item-context-menu"
-			ref={itemContextRef}
-			style={position}
-		>
-			<ul>
-				{
-					(contextMenuList || []).map((item, index) => {
-						if (item.type === 'separator') {
-							return <Divider key={index + 'separator'} />;
-						} else {
-							return (
-								<li
-									key={item.value}
-									className={cn({ disabled: item.disabled })}
-									onClick={() => {
-										if (!item.disabled) {
-											onContextMenuClick(item.value, props);
-											onCancel();
-										};
-									}}
-									style={{ color: item.color || 'inherit' }}
-								>
-									{item.icon || <i></i>}
-									<span>{item.label}</span>
-								</li>
-							);
-						}
-					})
-				}
-			</ul>
-		</div>
-	);
+  return (
+    <div
+      className="disk-item-context-menu"
+      ref={itemContextRef}
+      style={position}
+    >
+      <ul>
+        {(contextMenuList || []).map((item, index) => {
+          if (item.type === "separator") {
+            return (
+              <div
+                key={index + "separator"}
+                className="disk-menu-divider"
+              ></div>
+            );
+          } else {
+            return (
+              <li
+                key={item.value}
+                className={cn({ disabled: item.disabled })}
+                onClick={() => {
+                  if (!item.disabled) {
+                    onContextMenuClick(item.value, props);
+                    onCancel();
+                  }
+                }}
+                style={{ color: item.color || "inherit" }}
+              >
+                {item.icon || <i></i>}
+                <span>{item.label}</span>
+              </li>
+            );
+          }
+        })}
+      </ul>
+    </div>
+  );
 };
